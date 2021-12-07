f75bd24f6863c01710f2897820c73ab7893dbc42
sneha.poulose@rapidvaluesolutions.com <sneha.poulose@rapidvaluesolutions.com>
2021-12-07 15:13:33 +0530

    -- added a functionality to perform different mathematical operations.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function display(val) {
   document.getElementById("result").value += val;
 }
-function clearScreen(){
-    document.getElementById("result").value = " "
+function clearScreen() {
+  document.getElementById("result").value = " ";
+}
+function calculatorOperations() {
+  var userInput = document.getElementById("result").value;
+  var userResult = eval(userInput);
+  document.getElementById("result").value = userResult;
 }
